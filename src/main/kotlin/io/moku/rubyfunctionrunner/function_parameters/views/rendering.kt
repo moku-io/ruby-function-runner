9b3f780ddf9a411cf9f57b1252b7f9adacf082d7
Nicola Abbagnato <nicola.abbagnato@moku.io>
2024-06-28 17:32:27 +0200

    Parameter dialog fixes

--- src/main/kotlin/io/moku/rubyfunctionrunner/function_parameters/views/rendering.kt
+++ src/main/kotlin/io/moku/rubyfunctionrunner/function_parameters/views/rendering.kt
@@ -4,6 +4,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.openapi.ui.validation.DialogValidation
 import com.intellij.ui.EditorTextField
+import com.intellij.ui.JBColor
+import com.intellij.ui.SideBorder
 import com.intellij.ui.dsl.builder.*
 import io.moku.rubyfunctionrunner.function_parameters.models.ParameterModel
 import org.jetbrains.plugins.ruby.ruby.lang.RubyFileType
@@ -32,20 +34,24 @@ fun Panel.render(models: List<ParameterModel>, project: Project) {
             model.tableModel?.let { tableModel ->
                 topGap(TopGap.SMALL)
                 cell(JPanel(BorderLayout()).apply {
+                    border = SideBorder(JBColor.border(), SideBorder.TOP or SideBorder.LEFT)
+
                     val table = JTable(tableModel)
-                    val editor = RubyTableCellEditor(project)
-                    table.setDefaultEditor(String::class.java, editor)
+                    table.setDefaultEditor(String::class.java, RubyTableCellEditor(project))
                     table.rowHeight = 20
                     add(table, BorderLayout.CENTER)
-                    add(table.tableHeader, BorderLayout.NORTH)
-                }).widthGroup("input")
+
+                    val tableHeader = table.tableHeader
+                    tableHeader.border = SideBorder(JBColor.border(), SideBorder.RIGHT)
+                    add(tableHeader, BorderLayout.NORTH)
+                }).align(AlignX.FILL)
                 if (index + 1 < models.count()) {
                     bottomGap(BottomGap.SMALL)
                 }
             } ?: run {
                 cell(EditorTextField(project, RubyFileType.RUBY))
                     .bind(EditorTextField::getText, EditorTextField::setText, model::currentValue.toMutableProperty())
-                    .widthGroup("input")
+                    .align(AlignX.FILL)
                     .required()
             }
         }
