01fddae35e8aa736ade32ae6f44d31bb43f40bde
Nicola Abbagnato <nicola.abbagnato@moku.io>
2024-07-01 09:33:12 +0200

    Compatibility to 2024.1 ides

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.moku"
-version = "0.0.2"
+version = "241.0.1"
 
 repositories {
     mavenCentral()
@@ -19,13 +19,13 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2024.1.3")
+    version.set("2024.1.4")
     type.set("IU") // Target IDE Platform
 
     // Require the targetIDE plugin or library. Use the stable version
     // compatible with intellij.version and intellij.type specified above:
     // Dependent on ruby plugin (RubyMine / IntelliJ ultimate with Ruby plugin)
-    plugins.set(listOf("org.jetbrains.plugins.ruby:241.17890.1"))
+    plugins.set(listOf("org.jetbrains.plugins.ruby:241.18034.62"))
 }
 
 tasks {
@@ -40,7 +40,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("241")
-        untilBuild.set("242.*")
+        untilBuild.set("241.*")
     }
 
     signPlugin {
